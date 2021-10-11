69c8316a9c5924871e9e1dbbc3a6bbc14ec0f861
Tadhg <tadhg@pop-os.localdomain>
2021-10-11 16:35:39 +1300

    add base64 converter and random words

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,26 +2,66 @@
 
 pragma solidity 0.8.0;
 
+// We need some util functions for strings.
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
+
 
 contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() ERC721 ("MockGraduationNFT", "NiceWork") {
-    console.log("This is my NFT contract. Woah!");
+  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+  string[] firstWords = ["Slightly", "Somewhat", "Significantly", "Mostly", "Incredibly", "Absolutely"];
+  string[] secondWords = ["Magnificent", "Terrible", "Lovable", "Admirable", "Evil", "Holy"];
+  string[] thirdWords = ["Rose", "Sword", "Night", "Wind", "Dog", "Season"];
+
+  constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contract. Neato!");
+  }
+
+  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
+    rand = rand % firstWords.length;
+    return firstWords[rand];
+  }
+
+  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
+    rand = rand % secondWords.length;
+    return secondWords[rand];
+  }
+
+  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
+    rand = rand % thirdWords.length;
+    return thirdWords[rand];
+  }
+
+  function random(string memory input) internal pure returns (uint256) {
+      return uint256(keccak256(abi.encodePacked(input)));
   }
 
   function makeAnEpicNFT() public {
     uint256 newItemId = _tokenIds.current();
 
+    string memory first = pickRandomFirstWord(newItemId);
+    string memory second = pickRandomSecondWord(newItemId);
+    string memory third = pickRandomThirdWord(newItemId);
+
+    string memory finalSvg = string(abi.encodePacked(baseSvg, first, second, third, "</text></svg>"));
+    console.log("\n--------------------");
+    console.log(finalSvg);
+    console.log("--------------------\n");
+
     _safeMint(msg.sender, newItemId);
-
-    _setTokenURI(newItemId, "https://jsonkeeper.com/b/FBPI");
-    console.log('An NFT w/ ID %s has been minted to %s', newItemId, msg.sender);
-
+  
+    _setTokenURI(newItemId, "blah");
+  
     _tokenIds.increment();
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
   }
 }
